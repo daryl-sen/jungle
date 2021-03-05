65b3c486ecff9247fbe7f4ea4e92637232d49f16
daryl <tsdaryl@gmail.com>
2021-03-04 22:03:11 -0800

    remove unused format_number function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,18 +6,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-
-    def format_number(num_string)
-      num_string = num_string.to_f / 100
-      thousands = (num_string / 1000).to_i
-      if thousands != 0
-        return thousands + ',' + num_string
-      end
-      return num_string
-    end
-
-    # @product.price = format_number(@product.price)
-
     render 'show'
   end
 
